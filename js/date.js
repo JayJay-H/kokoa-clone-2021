e22b5c93fb2f80aaa47eed2408897e0adb022ae8
JayJay-H <0911aza@naver.com>
2021-07-13 20:20:38 +0900

    Add badge.js

--- js/date.js
+++ js/date.js
@@ -1,4 +1,5 @@
 const timeStamp = document.querySelector(".chat__timestamp");
+const JHTimeStamp = document.querySelectorAll(".message__time");
 
 const days = [
   "Sunday",
@@ -29,6 +30,10 @@ const day = days[new Date().getDay()];
 const month = months[new Date().getMonth()];
 const date = new Date().getDate();
 const year = new Date().getFullYear();
+const hour = String(new Date().getHours()).padStart(2, "0");
+const minute = String(new Date().getMinutes()).padStart(2, "0");
 
 const timeStampText = `${day}, ${month} ${date}, ${year}`;
 timeStamp.innerText = timeStampText;
+
+JHTimeStamp[0].innerText = JHTimeStamp[1].innerText = `${hour}:${minute}`;
